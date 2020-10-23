a189f47d8357237ba9616f67c178b13c59525603
lucasbarrossantos <lucas-barros28@hotmail.com>
2020-10-23 20:24:35 -0300

    Implementacao da seguranca

--- src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,7 +2,9 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class FornecedorApplication {
 
